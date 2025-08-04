9d1ca7362ac9a7f904238d257eb726f61d3a9a54
Chadee Fouad <58062696+MyProjectsPage@users.noreply.github.com>
2025-08-04 14:58:02 +0300

    Added UberHotels project

---
+++ Run Jupyter Lab.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Change directory to where you want to launch Jupyter Lab from
+REM This example sets C:\ as the starting directory for Jupyter Lab's file browser.
+cd C:\
+
+REM Run Jupyter Lab directly.
+REM This assumes 'jupyter' is in your system's PATH,
+REM or your Python installation's Scripts directory is in PATH.
+jupyter lab
+
+pause
